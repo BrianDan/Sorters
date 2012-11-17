ef93f225a3319646a819c7c4ab7c60e8fbd09e54
Brian Bergman <sylviachet@gmail.com>
2012-11-16 19:40:48 -0600

    MORE TESTS

--- main.cpp
+++ main.cpp
@@ -11,72 +11,72 @@ int main ()
 	int hundred[100];
 	int thousand[1000];
 	int tenK[10000];
-	int fiftyK[50000];
+	int fiftyK[100000];
 	
 	int MShundredArr[100];
 	int MSthousandArr[1000];
 	int MStenKArr[10000];
-	int MSfiftyKArr[50000];
+	int MSfiftyKArr[100000];
 	
 	int IShundredArr[100];
 	int ISthousandArr[1000];
 	int IStenKArr[10000];
-	int ISfiftyKArr[50000];
+	int ISfiftyKArr[100000];
 	
 	int BShundredArr[100];
 	int BSthousandArr[1000];
 	int BStenKArr[10000];
-	int BSfiftyKArr[50000];
+	int BSfiftyKArr[100000];
 	
 	int QShundredArr[100];
 	int QSthousandArr[1000];
 	int QStenKArr[10000];
-	int QSfiftyKArr[50000];
+	int QSfiftyKArr[100000];
 	
 	FillArray(hundred, 100);
 	FillArray(thousand, 1000);
 	FillArray(tenK, 10000);
-	FillArray(fiftyK, 50000);
+	FillArray(fiftyK, 100000);
 	
 	CopyArray(hundred, MShundredArr, 100);
 	CopyArray(thousand, MSthousandArr, 1000);
 	CopyArray(tenK, MStenKArr, 10000);
-	CopyArray(fiftyK, MSfiftyKArr, 50000);
+	CopyArray(fiftyK, MSfiftyKArr, 100000);
 	
 	CopyArray(hundred, IShundredArr, 100);
 	CopyArray(thousand, ISthousandArr, 1000);
 	CopyArray(tenK, IStenKArr, 10000);
-	CopyArray(fiftyK, ISfiftyKArr, 50000);
+	CopyArray(fiftyK, ISfiftyKArr, 100000);
 	
 	CopyArray(hundred, BShundredArr, 100);
 	CopyArray(thousand, BSthousandArr, 1000);
 	CopyArray(tenK, BStenKArr, 10000);
-	CopyArray(fiftyK, BSfiftyKArr, 50000);
+	CopyArray(fiftyK, BSfiftyKArr, 100000);
 	
 	CopyArray(hundred, QShundredArr, 100);
 	CopyArray(thousand, QSthousandArr, 1000);
 	CopyArray(tenK, QStenKArr, 10000);
-	CopyArray(fiftyK, QSfiftyKArr, 50000);
+	CopyArray(fiftyK, QSfiftyKArr, 100000);
 	
 	Sorter<int> MShundred (MShundredArr, 100);
 	Sorter<int> MSthousand (MSthousandArr, 1000);
 	Sorter<int> MStenK (MStenKArr, 10000);
-	Sorter<int> MSfiftyK (MSfiftyKArr, 50000);
+	Sorter<int> MSfiftyK (MSfiftyKArr, 100000);
 	
 	Sorter<int> IShundred (IShundredArr, 100);
 	Sorter<int> ISthousand (ISthousandArr, 1000);
 	Sorter<int> IStenK (IStenKArr, 10000);
-	Sorter<int> ISfiftyK (ISfiftyKArr, 50000);
+	Sorter<int> ISfiftyK (ISfiftyKArr, 100000);
 	
 	Sorter<int> BShundred (BShundredArr, 100);
 	Sorter<int> BSthousand (BSthousandArr, 1000);
 	Sorter<int> BStenK (BStenKArr, 10000);
-	Sorter<int> BSfiftyK (BSfiftyKArr, 50000);
+	Sorter<int> BSfiftyK (BSfiftyKArr, 100000);
 	
 	Sorter<int> QShundred (QShundredArr, 100);
 	Sorter<int> QSthousand (QSthousandArr, 1000);
 	Sorter<int> QStenK (QStenKArr, 10000);
-	Sorter<int> QSfiftyK (QSfiftyKArr, 50000);
+	Sorter<int> QSfiftyK (QSfiftyKArr, 100000);
 	
 	clock_t start = clock();
 	MShundred.MergeSort();
